be0df5d349640836f6983fa36762665a5f59ed11
Jonathan Cruz <jonathan.cruz@davinci.edu.ar>
2025-04-11 14:42:48 -0300

    Semana 04 - MVC

--- semana_04/controllers/userController.js
+++ semana_04/controllers/userController.js
@@ -6,11 +6,27 @@ const userModel = new UsersManager();
 const getUsers = async( request, response) => {
     try {
         const users = await userModel.getUsers();
-        response.status(200).json(users);
+        response.status(200).json({ msg: 'ok', data: users});
 
     } catch (error) {
         console.error({error});
-        response.status(500).json({error: 'Error del servidor'});
+        response.status(500).json({msg: 'Error del servidor', data: []});
+    }
+}
+
+const getUserById = async( request, response) => {
+    try {
+        //const id = request.params.id;
+        const { id } = request.params
+        const user = await userModel.getUserById(id);
+        if (user){
+            response.status(200).json({ msg: 'ok', data: user});
+        } else {
+            response.status(404).json({ msg: 'No se econtro el usuario', data: {}});
+        }
+    } catch (error) {
+        console.error({error});
+        response.status(500).json({msg: 'Error del servidor', data: []});
     }
 }
 
@@ -27,4 +43,35 @@ const setUser = async( request, response) =>{
     }
 }
 
-export { getUsers, setUser };
+const deleteUserById = async( request, response) => {
+    try {
+        const { id } = request.params
+        const status = await userModel.deleteUserById(id);
+        if (status){
+            response.status(200).json({ msg: 'Usuario eliminado', data: []});
+        } else {
+            response.status(404).json({ msg: 'No se econtro el usuario', data: []});
+        }
+    } catch (error) {
+        console.error({error});
+        response.status(500).json({msg: 'Error del servidor', data: []});
+    }
+}
+
+const updateUserById = async( request, response) => {
+    try {
+        const { id } = request.params
+        const user = request.body;
+
+        const status = await userModel.updateUserById(id, user);
+        if (status){
+            response.status(200).json({ msg: 'Usuario actualizado', data: user});
+        } else {
+            response.status(404).json({ msg: 'No se econtro el usuario', data: user});
+        }
+    } catch (error) {
+        console.error({error});
+        response.status(500).json({msg: 'Error del servidor', data: []});
+    }
+}
+export { getUsers, getUserById, setUser, deleteUserById, updateUserById };
--- semana_04/routers/usersRouter.js
+++ semana_04/routers/usersRouter.js
@@ -1,10 +1,14 @@
 import express from "express";
-import {getUsers, setUser} from "../controllers/userController.js";
+import {getUsers, getUserById, setUser, updateUserById, deleteUserById} from "../controllers/userController.js";
 
 const router = express.Router();
 
 // Definimos las rutas
 router.get('/', getUsers);
+router.get('/:id', getUserById);
 router.post('/', setUser);
+router.put('/:id', updateUserById);
+router.delete('/:id', deleteUserById);
+
 
 export default router;
